4737d6b9d484a1058bccfd49db62cf1ecc945fb6
Selim <selimabdelmonsef@gmail.com>
2021-06-21 19:26:02 +0200

    fixed web style for landing page

--- src/pages/cart_page/cart_page.js
+++ src/pages/cart_page/cart_page.js
@@ -4,29 +4,31 @@ import { connect } from "react-redux";
 import { _GetCartData } from "../../redux/redux-actions/cartData_actions";
 
 class CartPage extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         console.log(this.props.cartData);
-        
+
     }
     render() {
         return (
             <div>
-                <h1>Your Order</h1>
-                        {this.props.cartData?.data?.map((element, index) => {
-                            return <div>
-                                <div className={styles.product_lists_container}>
-                                    <p className={styles.productNameStyle}>{element.productName}</p>
-                                    <p className={styles.products_style}>{element.price}$</p>
-                                    <div>
-                                        <img className={styles.productImage} src={element.img} alt="" />
-                                        <button className={styles.addToCartBtn}>Remove</button>
-                                    </div>
-                                </div>
-                                <hr />
+                <h1 className={styles.yourOrder}>Your Order</h1>
+                <hr />
+                {this.props.cartData?.data?.map((element, index) => {
+                    return <div>
+                        <div className={styles.product_lists_container}>
+                            <p className={styles.productNameStyle}>{element.productName}</p>
+                            <p className={styles.products_style}>{element.price}$</p>
+                            <div>
+                                <img className={styles.productImage} src={element.img} alt="" />
+                                <button className={styles.addToCartBtn}>Remove</button>
                             </div>
-                        })}
-                        <div>Total Cost: {this.props.cartData.totalOrder}$</div>
+                        </div>
+                        <hr />
                     </div>
+                })}
+                <div className={styles.totalCost}>Total Cost: {this.props.cartData.totalOrder}$</div>
+                <button onClick={() => this.props.history.push("/")} className={styles.continueShopping}>CONTINUE SHOPPING</button>
+            </div>
         )
     }
 }
